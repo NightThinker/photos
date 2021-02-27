bb9306636ab2d877d8a52cfce07035904df8ffd6
NightThinker <travel_special@hotmail.com>
2021-02-27 23:05:39 +0700

    layout

--- src/shared/theme/Layout/Layout.js
+++ src/shared/theme/Layout/Layout.js
@@ -1,7 +1,7 @@
 import Search from '../../components/Search/Search'
 const Layout = ({ children, search, onChange }) => {
   return (
-    <ul className='flex flex-col p-10'>
+    <ul className='flex flex-col px-10 pt-1 pb-10'>
       <li><Search value={search} onChange={onChange} /></li>
       <li className='mt-7'>
         {children}
